5d1b76970dec52989d664b19ccd7d19441b60a7e
MalsorAbdyli <malsor.abdyli@student.uni-pr.edu>
2023-11-30 00:03:21 +0100

    update

--- Sockets/Server.java
+++ Sockets/Server.java
@@ -1,2 +1,17 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
+
 public class Server {
+
+    private static final int port = 2911;
+    private static final String host = "localhost";
+
 }
